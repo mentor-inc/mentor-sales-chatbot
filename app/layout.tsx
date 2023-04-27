e4bccc2ab62b65c3634eea0b322517364ac6edbd
Raibima Putra <rayp1100@gmail.com>
2023-04-27 13:58:09 +0700

    Change site metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "Sales Trainer by Mentor Inc.",
+  description: "Your personal sales simulation trainer by Mentor Inc.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "SalesTrainer",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
